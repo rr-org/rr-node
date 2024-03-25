1cdf53cb11ec051281d24dc48eaca6388b8bdf98
NubieMe <fahmi.mellow@gmail.com>
2024-03-25 10:57:30 +0700

    Revert "update: change file index to server"
    This reverts commit e46a1e8888b3f4ff6843c422f61b0a63a328132c.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
 import * as express from "express";
-import{createServer} from "http";
+import { createServer} from "http";
 import { Server } from "socket.io";
 import * as cors from "cors";
 import { IUsers } from "./utils/type";
@@ -11,8 +9,6 @@ const app = express();
 const server = createServer(app);
 app.use(cors());
 
-export const users: IUsers[] = [];
-
 export const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
@@ -20,65 +16,63 @@ export const io = new Server(server, {
     },
 });
 
-AppDataSource.initialize().then(async () => {
-    
-    io.on("connection", (socket) => {
+export const users: IUsers[] = [];
 
-        socket.on("joinRoom", ({ username }) => {
-            let user;
-    
-            if (users.length === 0 || users.length === 5) {
-                user = {
-                    id: socket.id,
-                    username,
-                    room: socket.id,
-                };
-    
-                users.push(user);
-                socket.join(user.room);
-                countdown(user.room);
-    
-            } else if (users.length > 0 && users.length < 5) {
-                user = {
-                    id: socket.id,
-                    username,
-                    room: users[0].room,
-                };
-    
-                users.push(user);
-                socket.join(users[0].room);
-            } else {
-                user = {
-                    id: socket.id,
-                    username,
-                    room: users[5].room,
-                };
-    
-                users.push(user);
-                socket.join(users[5].room);
-            }
-            
-            socket.broadcast.emit("usersList", users);
-            socket.emit("usersList", users);
-        });
-    
-        socket.on("sendAnswer", (data) => {
-            io.to(data.room).emit("receiveAnswer", data);
-        });
-    
-        socket.on("leaveRoom", ({ username, room }) => {
-            const i = JSON.stringify(users.findIndex((val) => val.username === username));
-    
-            socket.leave(room);
-            users.splice(Number(i), 1);
-    
-            io.emit("usersList", users);
-        });
-    });
-    
-    server.listen(3210, () => {
-        console.log("Server is running!");
-    });
-    
 
-}).catch(error => console.log(error))
+io.on("connection", (socket) => {
+
+    socket.on("joinRoom", ({ username }) => {
+        let user;
+
+        if (users.length === 0 || users.length === 5) {
+            user = {
+                id: socket.id,
+                username,
+                room: socket.id,
+            };
+
+            users.push(user);
+            socket.join(user.room);
+            countdown(user.room);
+
+        } else if (users.length > 0 && users.length < 5) {
+            user = {
+                id: socket.id,
+                username,
+                room: users[0].room,
+            };
+
+            users.push(user);
+            socket.join(users[0].room);
+        } else {
+            user = {
+                id: socket.id,
+                username,
+                room: users[5].room,
+            };
+
+            users.push(user);
+            socket.join(users[5].room);
+        }
+        
+        socket.broadcast.emit("usersList", users);
+        socket.emit("usersList", users);
+    });
+
+    socket.on("sendAnswer", (data) => {
+        io.to(data.room).emit("receiveAnswer", data);
+    });
+
+    socket.on("leaveRoom", ({ username, room }) => {
+        const i = JSON.stringify(users.findIndex((val) => val.username === username));
+
+        socket.leave(room);
+        users.splice(Number(i), 1);
+
+        io.emit("usersList", users);
+    });
+});
+
+server.listen(3210, () => {
+    console.log("Server is running!");
+});
